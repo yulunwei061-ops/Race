36f875f147c84028da0ff66335172d2f20287a66
yulunwei061-ops <yulunwei061@gmail.com>
2025-11-10 16:24:56 +0800

    v.4 拖曳

--- app/src/main/java/tw/edu/pu/csim/tcyang/race/GameScreen.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/race/GameScreen.kt
@@ -2,13 +2,16 @@ package tw.edu.pu.csim.tcyang.race
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 
 @Composable
 fun GameScreen(message: String, gameViewModel: GameViewModel) {
@@ -18,17 +21,32 @@ fun GameScreen(message: String, gameViewModel: GameViewModel) {
         .background(Color.Yellow)
     ){
         Canvas (modifier = Modifier.fillMaxSize()
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    change.consume() // 告訴系統已經處理了這個事件
+                    gameViewModel.MoveCircle( dragAmount.x, dragAmount.y)
+                }
+            }
+
+
         ) {
             // 繪製圓形
             drawCircle(
                 color = Color.Red,
                 radius = 100f,
-                center = Offset(100f, 100f)
+                center = Offset(gameViewModel.circleX, gameViewModel.circleY)
             )
         }
 
 
         Text(text = message + gameViewModel.screenWidthPx.toString() + "*"
                 + gameViewModel.screenHeightPx.toString())
+
+        Button(onClick = {gameViewModel.gameRunning = true
+            gameViewModel.StartGame()
+        }
+        ){
+           Text("遊戲開始")
+        }
     }
 }
