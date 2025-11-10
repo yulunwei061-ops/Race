e01b1d45060746a300e7bcc3375e98dac82cf537
yulunwei061-ops <yulunwei061@gmail.com>
2025-11-10 15:51:31 +0800

    v.3 紅色圓形

--- app/src/main/java/tw/edu/pu/csim/tcyang/race/GameScreen.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/race/GameScreen.kt
@@ -1,19 +1,33 @@
 package tw.edu.pu.csim.tcyang.race
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 
 @Composable
 fun GameScreen(message: String, gameViewModel: GameViewModel) {
+
     Box(modifier = Modifier
         .fillMaxSize()
         .background(Color.Yellow)
     ){
+        Canvas (modifier = Modifier.fillMaxSize()
+        ) {
+            // 繪製圓形
+            drawCircle(
+                color = Color.Red,
+                radius = 100f,
+                center = Offset(100f, 100f)
+            )
+        }
+
+
         Text(text = message + gameViewModel.screenWidthPx.toString() + "*"
                 + gameViewModel.screenHeightPx.toString())
     }
