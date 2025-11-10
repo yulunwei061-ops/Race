ced871c3a86c58bbac71ae88ac8606919442ffd7
yulunwei061-ops <yulunwei061@gmail.com>
2025-11-10 14:59:32 +0800

    v.1橫式螢幕，隱藏狀態列

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4") // 請檢查最新版本
+
 }
